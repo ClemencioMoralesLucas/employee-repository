90c1d0d0f9f14a9110ad0d94798149f9b9aaec85
ClemencioMoralesLucas <clemenciomorales.lucas@tesco.com>
2021-04-24 11:59:57 +0200

    Includes a HierarchicalOrganization to create top-down/bottom-up enterprise hierarchies.

---
+++ src/main/java/com/personio/employeehierarchy/domain/HierarchicalOrganization.java
@@ -0,0 +1,21 @@
+package com.personio.employeehierarchy.domain;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
+
+public class HierarchicalOrganization {
+
+    public static Map<String, Map> assembleBottomUpHierarchy(final Employee employee) {
+        final var optionalManager = employee.getManager();
+        return optionalManager.<Map<String, Map>>map(value ->
+                Map.of(employee.getName(), assembleBottomUpHierarchy(value)))
+                .orElseGet(() -> Map.of(employee.getName(), Map.of()));
+    }
+
+    public static Map<String, Map> assembleTopDownHierarchy(final List<Employee> employeeList) {
+        return employeeList.isEmpty() ? Map.of() : employeeList.stream().collect(
+                toMap(Employee::getName, employee -> assembleTopDownHierarchy(employee.getManagedEmployees())));
+    }
+}
