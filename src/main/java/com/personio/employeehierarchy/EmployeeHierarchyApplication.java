8d19160c9ad7622d28553c00081a34658395fb74
ClemencioMoralesLucas <clemenciomorales.lucas@tesco.com>
2021-04-26 20:09:49 +0200

    Updates base-packages scan to avoid auto-detection mistakes.

--- src/main/java/com/personio/employeehierarchy/EmployeeHierarchyApplication.java
+++ src/main/java/com/personio/employeehierarchy/EmployeeHierarchyApplication.java
@@ -3,7 +3,7 @@ package com.personio.employeehierarchy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.personio.employeehierarchy"})
 public class EmployeeHierarchyApplication {
 
 	public static void main(String[] args) {
